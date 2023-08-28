40576b20b96636bc591f2c933475bcf525f907d8
Ravi Sinha <r.sinha@cabi.org>
2023-08-28 15:54:45 +0530

    Enable logging

--- insightreportingml.job/core/logging.py
+++ insightreportingml.job/core/logging.py
@@ -2,7 +2,8 @@ import seqlog
 import logging
 from core.config import settings
 
-seqlog.log_to_seq(
+def initialise_seq():
+   seqlog.log_to_seq(
    server_url= settings.SEQ_SERVER,
    api_key= settings.SEQ_API_KEY,
    level=logging.INFO,
--- insightreportingml.job/main.py
+++ insightreportingml.job/main.py
@@ -1,16 +1,30 @@
+import sys
 import logging
-import core.logging
+from core.logging import initialise_seq
 from scripts.retraining import retraining
 
-# Read training data
-data = retraining.read_from_excel()
+initialise_seq()
 
-# Generate models from training data
-[acc, roc_auc, lr_tfidf, tfidf_vectorizer, rfc_model,
-    preprocessing] = retraining.generate_models(data)
+try:
+    # Read training data
+    data = retraining.read_from_excel()
 
-print(acc, roc_auc)
+    # Generate models from training data
+    [acc, roc_auc, lr_tfidf, tfidf_vectorizer, rfc_model,
+        preprocessing] = retraining.generate_models(data)
 
-# Serilized training model to disk
-retraining.save_models('3', lr_tfidf, tfidf_vectorizer, rfc_model,
-                       preprocessing)
+    print(acc, roc_auc)
+
+    # Serilized training model to disk
+    retraining.save_models('3', lr_tfidf, tfidf_vectorizer, rfc_model,
+                           preprocessing)
+    logging.info(
+        'Training models generated successfully-{acc}:{roc_auc}', acc, roc_auc)
+    
+    sys.exit(0)
+
+except Exception as e:
+    logging.error('Error while generating training models: ' + str(e))
+    sys.exit(1)
+
+
