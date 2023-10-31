e6a0ce05bc8b8a9163b04ebbe8f571879ffa6bc4
Deepak Mallick <d.Mallick@cabi.org>
2023-10-31 15:29:58 +0000

    Merged PR 2632: #61724 - update helm
    #61724 - update helm
    
    Related work items: #60413, #61724

--- insightreportingml.job/core/config.py
+++ insightreportingml.job/core/config.py
@@ -7,8 +7,7 @@ class ApplicationSettings(BaseSettings):
     SEQ_API_KEY: str = Field(default='',env='SEQ_API_KEY')
     SEQ_SERVER: str = Field(default='',env='SEQ_SERVER')
     MLMODEL_DIRECTORY: str = Field(default='', env='FileStoreSettings__StorageDirectory')
-    CONN_STRING: str = Field(default='', env='ConnectionStrings__insightreporting')
-    DB_SERVER: str = Field(default='', env='SQLServerInstance')
+    DB_SERVER: str = Field(default='', env='InsightReporting_DB_SERVER')
     DB_NAME: str = Field(default='', env='InsightReporting_DB_NAME')
     DB_UID: str = Field(default='', env='InsightReporting_DB_UID')
     DB_PWD: str = Field(default='', env='InsightReporting_DB_PWD')
@@ -29,39 +28,14 @@ class Settings(ApplicationSettings):
         case_sensitive = True
 
     @property
-    def parsed_connection_string(self):
-        components = dict(item.split('=') for item in self.CONN_STRING.split(';') if item)
-        for key in components:
-            if components[key]:
-                print(f"{key.capitalize()} value retrieved.")
-        return {
-            'database': components.get('database', ''),
-            'username': components.get('username', ''),
-            'password': components.get('password', ''),
-            'server': components.get('server', '')
-        }
-
-    @property
     def pyodbc_connection_string(self):
-        if self.CONN_STRING:
-            print("connection string retrived")
-            parsed = self.parsed_connection_string
-            if parsed['server']:
-                print(f"DB Server: {parsed['server']}")
+        if self.DB_SERVER:
+            print(f"DB Server: {self.DB_SERVER}")
 
-            return (f"DRIVER={self.DB_DRIVER};"
-                    f"SERVER={parsed['server']};"
-                    f"DATABASE={parsed['database']};"
-                    f"UID={parsed['username']};"
-                    f"PWD={parsed['password']};")
-        else:
-            if self.DB_SERVER:
-                print(f"DB Server: {self.DB_SERVER}")
-
-            return (f"DRIVER={self.DB_DRIVER};"
-                    f"SERVER={self.DB_SERVER};"
-                    f"DATABASE={self.DB_NAME};"
-                    f"UID={self.DB_UID};"
-                    f"PWD={self.DB_PWD};")
+        return (f"DRIVER={self.DB_DRIVER};"
+                f"SERVER={self.DB_SERVER};"
+                f"DATABASE={self.DB_NAME};"
+                f"UID={self.DB_UID};"
+                f"PWD={self.DB_PWD};")
 
 settings = Settings()
