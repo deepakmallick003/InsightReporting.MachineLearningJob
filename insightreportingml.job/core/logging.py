49052e0f6968c46ef4b8b43437b40b4af15f104c
Deepak Mallick <d.Mallick@cabi.org>
2023-11-06 15:48:55 +0000

    Updated logging.py

--- insightreportingml.job/core/logging.py
+++ insightreportingml.job/core/logging.py
@@ -8,7 +8,7 @@ class SeqLogging:
             seqlog.log_to_seq(
                   server_url= settings.SEQ_SERVER,
                   api_key= settings.SEQ_API_KEY,
-                  level=logging.DEBUG,
+                  level=logging.INFO,
                   batch_size=1,
                   auto_flush_timeout=10, 
                   override_root_logger=True,
