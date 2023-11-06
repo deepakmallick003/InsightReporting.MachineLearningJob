4aaf5d0e0ce68c1ab8c92fc3ee1c2f724ad36b77
Deepak Mallick <D.Mallick@cabi.org>
2023-11-06 16:04:15 +0000

    #61724 - test simple logging
    Related Work Items: #60413

--- insightreportingml.job/core/logging.py
+++ insightreportingml.job/core/logging.py
@@ -2,10 +2,7 @@ import seqlog
 import logging
 from core.config import settings
 
-class SeqLogging:
-
-      def __init__(self):
-            seqlog.log_to_seq(
+seqlog.log_to_seq(
                   server_url= settings.SEQ_SERVER,
                   api_key= settings.SEQ_API_KEY,
                   level=logging.INFO,
@@ -14,8 +11,3 @@ class SeqLogging:
                   override_root_logger=True,
                   support_extra_properties=True
             )
-                  
-            root_logger = logging.getLogger()
-            console_handler = logging.StreamHandler()
-            console_handler.setLevel(logging.INFO)
-            root_logger.addHandler(console_handler)
--- insightreportingml.job/main.py
+++ insightreportingml.job/main.py
@@ -1,98 +1,100 @@
 import logging
 import time
 
-from core.logging import SeqLogging 
-from scripts.retraining import ReTraining
-from scripts.db import DatabaseManager
+import core.logging
 
-def retraining_job():   
-    try:
-        logging.info('----------------------')
-        print('----------------------')
+# from core.logging import SeqLogging 
+# from scripts.retraining import ReTraining
+# from scripts.db import DatabaseManager
 
-        logging.info("Retraining Job Iteration Started")
-        print("Retraining Job Iteration Started")
+# def retraining_job():   
+#     try:
+#         logging.info('----------------------')
+#         print('----------------------')
 
-        with DatabaseManager() as db_instance:
-            logging.info('Fetching Unprocessed Model Versions')
-            print('Fetching Unprocessed Model Versions')
-            unprocessed_model_versions = db_instance.get_unprocessed_model_versions()
-            if unprocessed_model_versions:
-                logging.info('Unprocessed Models Versions found, Starting Retraining')
-                print('Unprocessed Models Versions found, Starting Retraining')
+#         logging.info("Retraining Job Iteration Started")
+#         print("Retraining Job Iteration Started")
 
-                best_models_meta= {
-                    'rfc_model_version_id': None,
-                    'rfc_model_id': None,
-                    'lr_model_version_id': None,
-                    'lr_model_id': None                    
-                }
+#         with DatabaseManager() as db_instance:
+#             logging.info('Fetching Unprocessed Model Versions')
+#             print('Fetching Unprocessed Model Versions')
+#             unprocessed_model_versions = db_instance.get_unprocessed_model_versions()
+#             if unprocessed_model_versions:
+#                 logging.info('Unprocessed Models Versions found, Starting Retraining')
+#                 print('Unprocessed Models Versions found, Starting Retraining')
 
-                for row in unprocessed_model_versions:
-                    if row.ModelID == 1:  # RFC model
-                        best_models_meta['rfc_model_version_id'] = row.ModelVersionID
-                        best_models_meta['rfc_model_id'] = row.ModelID
-                    elif row.ModelID == 2:  # LR model
-                        best_models_meta['lr_model_version_id'] = row.ModelVersionID
-                        best_models_meta['lr_model_id'] = row.ModelID
+#                 best_models_meta= {
+#                     'rfc_model_version_id': None,
+#                     'rfc_model_id': None,
+#                     'lr_model_version_id': None,
+#                     'lr_model_id': None                    
+#                 }
 
-                if None in best_models_meta.values():
-                    logging.error("One or more model versions were not assigned correctly.")
-                else:
-                    logging.info('Fetching Training Data from DB')
-                    unprocessed_training_data = db_instance.get_training_data()
-                    if unprocessed_training_data:
-                        logging.info('Initiating Retraining Process')
-                        print('Initiating Retraining Process')
+#                 for row in unprocessed_model_versions:
+#                     if row.ModelID == 1:  # RFC model
+#                         best_models_meta['rfc_model_version_id'] = row.ModelVersionID
+#                         best_models_meta['rfc_model_id'] = row.ModelID
+#                     elif row.ModelID == 2:  # LR model
+#                         best_models_meta['lr_model_version_id'] = row.ModelVersionID
+#                         best_models_meta['lr_model_id'] = row.ModelID
 
-                        retraining_instance = ReTraining()
+#                 if None in best_models_meta.values():
+#                     logging.error("One or more model versions were not assigned correctly.")
+#                 else:
+#                     logging.info('Fetching Training Data from DB')
+#                     unprocessed_training_data = db_instance.get_training_data()
+#                     if unprocessed_training_data:
+#                         logging.info('Initiating Retraining Process')
+#                         print('Initiating Retraining Process')
+
+#                         retraining_instance = ReTraining()
                         
-                        best_models_and_data = retraining_instance.train_models(unprocessed_training_data, best_models_meta)
-                        if None in best_models_and_data:
-                            logging.error('One or more model retraining failed')
-                            print('One or more model retraining failed')
+#                         best_models_and_data = retraining_instance.train_models(unprocessed_training_data, best_models_meta)
+#                         if None in best_models_and_data:
+#                             logging.error('One or more model retraining failed')
+#                             print('One or more model retraining failed')
 
-                            db_instance.save_models_to_db(best_models_and_data, processing_status = 51) # status failed
-                        else:
-                            logging.info('Retraining Process Complete')
-                            print('Retraining Process Complete')
+#                             db_instance.save_models_to_db(best_models_and_data, processing_status = 51) # status failed
+#                         else:
+#                             logging.info('Retraining Process Complete')
+#                             print('Retraining Process Complete')
 
-                            logging.info("Saving Best Models to Database")
-                            print("Saving Best Models to Database")
+#                             logging.info("Saving Best Models to Database")
+#                             print("Saving Best Models to Database")
 
-                            db_instance.save_models_to_db(best_models_and_data, processing_status = 1)# status processed
+#                             db_instance.save_models_to_db(best_models_and_data, processing_status = 1)# status processed
                             
-                            logging.info("Succesfully Saved Models to Database")
-                            print("Succesfully Saved Models to Database")
-                    else:
-                        logging.info("No training data available for retraining, Iteration Skipped")
-                        print("No training data available for retraining, Iteration Skipped")
-            else:
-                logging.info('No Unprocessed Model Versions found, Iteration Skipped')
-                print('No Unprocessed Model Versions found, Iteration Skipped')
+#                             logging.info("Succesfully Saved Models to Database")
+#                             print("Succesfully Saved Models to Database")
+#                     else:
+#                         logging.info("No training data available for retraining, Iteration Skipped")
+#                         print("No training data available for retraining, Iteration Skipped")
+#             else:
+#                 logging.info('No Unprocessed Model Versions found, Iteration Skipped')
+#                 print('No Unprocessed Model Versions found, Iteration Skipped')
 
-        logging.info("Retraining Job Iteration Complete.")
-        print("Retraining Job Iteration Complete.")
+#         logging.info("Retraining Job Iteration Complete.")
+#         print("Retraining Job Iteration Complete.")
 
-        logging.info('----------------------')
-        print('----------------------')
-    except Exception as e:
-        logging.error('Retraining Retraining Iteration Failed with error: ' + str(e))
-        logging.info('----------------------')
+#         logging.info('----------------------')
+#         print('----------------------')
+#     except Exception as e:
+#         logging.error('Retraining Retraining Iteration Failed with error: ' + str(e))
+#         logging.info('----------------------')
 
-        print('Retraining Retraining Iteration Failed with error: ' + str(e))
-        print('----------------------')
+#         print('Retraining Retraining Iteration Failed with error: ' + str(e))
+#         print('----------------------')
 
 
 if __name__ == "__main__":
 
-    SeqLogging() 
+    # SeqLogging()
     
     logging.info("Insight job is running")
     print('Insight job is running')
     
-    retraining_job()  
+    # retraining_job()  
        
     # Interval time in seconds
-    interval = 60 * 5  
+    interval = 60
     time.sleep(interval)
